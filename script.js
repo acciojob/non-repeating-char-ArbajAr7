d451e80e6234ea88bfe4d30ca0ccf572faf3190a
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-06 02:04:22 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const charCountMap = {};
+  
+  // Count the occurrences of each character in the string
+  for (let char of str) {
+    if (charCountMap[char]) {
+      charCountMap[char]++;
+    } else {
+      charCountMap[char] = 1;
+    }
+  }
+  for( key in charCountMap){
+      if(charCountMap[key] == 1)
+      {
+          return key;
+      }
+  }
+  return null;
 }
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
